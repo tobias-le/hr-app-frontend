17dc4d95481030e2ae1df1761da570034567d6b7
Tobias Le <letobias@fel.cvut.cz>
2024-10-14 17:23:15 +0200

    deleted code duplications

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,27 +11,13 @@ import {
   AppBar,
   Toolbar,
   Button,
-  createTheme,
-  ThemeProvider,
   IconButton,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import { orange, grey } from "@mui/material/colors";
 import { menuItems } from "../config/menuItems";
 
 const drawerWidth = 240;
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: orange[400],
-    },
-    secondary: {
-      main: grey[200],
-    },
-  },
-});
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -46,101 +32,99 @@ export default function Layout({ children }: LayoutProps) {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex", minHeight: "100vh" }}>
-        <AppBar
-          position="fixed"
-          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        >
-          <Toolbar sx={{ justifyContent: "space-between" }}>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <IconButton
-                color="inherit"
-                aria-label="toggle drawer"
-                edge="start"
-                onClick={toggleDrawer}
-                sx={{ mr: 2 }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ display: "flex", alignItems: "center" }}
-              >
-                <BusinessCenterIcon
-                  sx={{
-                    color: "white",
-                    bgcolor: "black",
-                    width: 24,
-                    height: 24,
-                    borderRadius: "50%",
-                    p: 0.5,
-                    mr: 1,
-                  }}
-                />
-                HR Tool
-              </Typography>
-            </Box>
-            <Box>
-              <Button color="inherit">Profile</Button>
-              <Button color="inherit">Log out</Button>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="persistent"
-          open={isDrawerOpen}
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            [`& .MuiDrawer-paper`]: {
-              width: drawerWidth,
-              boxSizing: "border-box",
-              bgcolor: "primary.main",
-              color: "white",
-            },
-          }}
-        >
-          <Toolbar />
-          <Box sx={{ overflow: "auto", mt: 2 }}>
-            <Typography variant="h6" sx={{ px: 2, mb: 2 }}>
-              Menu
+    <Box sx={{ display: "flex", minHeight: "100vh" }}>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <IconButton
+              color="inherit"
+              aria-label="toggle drawer"
+              edge="start"
+              onClick={toggleDrawer}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: "flex", alignItems: "center" }}
+            >
+              <BusinessCenterIcon
+                sx={{
+                  color: "white",
+                  bgcolor: "black",
+                  width: 24,
+                  height: 24,
+                  borderRadius: "50%",
+                  p: 0.5,
+                  mr: 1,
+                }}
+              />
+              HR Tool
             </Typography>
-            <List>
-              {menuItems.map((item) => (
-                <ListItem key={item.text} disablePadding>
-                  <ListItemButton
-                    component={Link}
-                    to={item.path}
-                    selected={location.pathname === item.path}
-                    sx={{
-                      "&.Mui-selected": {
-                        bgcolor: "secondary.main",
-                        color: "text.primary",
-                      },
-                    }}
-                  >
-                    <ListItemText primary={item.text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
           </Box>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            p: 3,
-            marginLeft: isDrawerOpen ? `${drawerWidth}px` : 0,
-            transition: "margin 0.2s",
-          }}
-        >
-          <Toolbar />
-          {children}
+          <Box>
+            <Button color="inherit">Profile</Button>
+            <Button color="inherit">Log out</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="persistent"
+        open={isDrawerOpen}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            bgcolor: "primary.main",
+            color: "white",
+          },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: "auto", mt: 2 }}>
+          <Typography variant="h6" sx={{ px: 2, mb: 2 }}>
+            Menu
+          </Typography>
+          <List>
+            {menuItems.map((item: { text: string; path: string }) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton
+                  component={Link}
+                  to={item.path}
+                  selected={location.pathname === item.path}
+                  sx={{
+                    "&.Mui-selected": {
+                      bgcolor: "secondary.main",
+                      color: "text.primary",
+                    },
+                  }}
+                >
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
         </Box>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          marginLeft: isDrawerOpen ? `${drawerWidth}px` : 0,
+          transition: "margin 0.2s",
+        }}
+      >
+        <Toolbar />
+        {children}
       </Box>
       <Box
         component="footer"
@@ -153,6 +137,6 @@ export default function Layout({ children }: LayoutProps) {
       >
         © 2024 HR Tool - All Rights Reserved
       </Box>
-    </ThemeProvider>
+    </Box>
   );
 }
