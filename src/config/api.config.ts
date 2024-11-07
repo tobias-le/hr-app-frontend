43cce591c6b56756e4584e4c14e751f38920ad83
Tobias Le <letobias@fel.cvut.cz>
2024-11-07 11:42:46 +0100

    revert url of api

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,7 @@
 const API_CONFIG = {
-  // BASE_URL: "https://backend-test-7dda.up.railway.app",
-  BASE_URL: "http://localhost:8080",
+  BASE_URL: "https://backend-test-7dda.up.railway.app",
+  // BASE_URL: "http://localhost:8080",
+
   ENDPOINTS: {
     EMPLOYEES: "/api/employees",
     ATTENDANCE_SUMMARY: "/api/attendance/summary",
